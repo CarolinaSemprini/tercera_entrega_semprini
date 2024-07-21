fda5ba52bd5b8c5d21b70e687d7825900c3d172d
carolina semprini <carolinasemprini@gmail.com>
2024-07-20 21:11:29 -0300

    cuarta entrega

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -3,6 +3,7 @@ export const productsRouter = express.Router();
 import { productsController } from "../controllers/products.controller.js";
 import { checkLogin, checkAdmin } from "../middlewares/main.js";
 
+
 productsRouter.get("/", productsController.readByRenderUser);
 // Rutas para productos que requieren autenticación y roles específicos
 productsRouter.get("/", checkLogin, productsController.readByRenderUser);
